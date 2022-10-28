e40441dfd411b359f16b5120663ac7f2dedb0df3
RahulGaur998 <gaur.rahul996@gmail.com>
2022-10-28 18:01:37 +0530

    ADD: Added logic to count entries

--- src/main/java/com/employee/employeepayrollservice/EmployeePayrollService.java
+++ src/main/java/com/employee/employeepayrollservice/EmployeePayrollService.java
@@ -37,6 +37,12 @@ public class EmployeePayrollService {
             new EmployeePayrollFileIOService().printData();
     }
 
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.FILE_IO))
+            return new EmployeePayrollFileIOService().countEntries();
+        return 0;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Payroll Service program");
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
